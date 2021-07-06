855d9bec90b197d1096590b3e6614b05a31b5b3c
Moises Hernandez <mhdezcoronado@gmail.com>
2021-07-06 15:24:40 +0200

    Add display method

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
 let books = []
 let counter = 0
+const bookList = document.getElementById('bookList');
+let savedBooks = localStorage.getItem('savedBooks')
+
 
 function addBook(event){
     event.preventDefault()
     
-    let title = event.target['title'].value
-    let author = event.target['author'].value
-
+    let title = document.getElementById('title')
+    let author = document.getElementById('author')
+    
     books.push({title: title.value, author: author.value, id: counter+=1})
     
     showBooks()
-    
     localStorage.setItem('savedBooks', JSON.stringify(books))
-
+    title.value = ''
+    author.value = ''
 }
+
+if(savedBooks){
+    books = JSON.parse(savedBooks)
+    showBooks()
+}
+
 
 function showBooks(){
-        bookList.innerHTML = ''
-        books.forEach((book) => {
-            display( book )
-        })
-    }
- 
+    bookList.innerHTML = ''
+    books.forEach((book) => {
+        display( book )
+    })
 }
 
+function display({title, author, id }){
 
-console.log(books);
+    let li = document.createElement('li')
+    let titleP = document.createElement('p')
+    let authorP = document.createElement('p')
+    let button = document.createElement('button')
+
+    titleP.innerHTML = title
+    authorP.innerHTML = author
+    button.innerHTML = 'remove'
+    button.className = 'remove'
+    button.addEventListener('click', function(){
+        books = books.filter((obj) => obj.id !== id)
+        bookList.innerHTML = ''
+        books.forEach((book) => {
+            showBooks(book)
+        })
+        localStorage.setItem('savedBooks', JSON.stringify(books))
+    })
+
+    li.appendChild(titleP)
+    li.appendChild(authorP)
+    li.appendChild(button)
+    bookList.appendChild(li)
+}
