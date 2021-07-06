463432e9d4e4fb39090c678a9589d593a24471fb
Moises Hernandez <mhdezcoronado@gmail.com>
2021-07-06 10:39:18 +0200

    Update addBook method

--- main.js
+++ main.js
@@ -6,32 +6,13 @@ function addBook(event){
     let title = event.target['title'].value
     let author = event.target['author'].value
 
-    let book = {title: title, author: author}
+    books.push({title: title, author: author})
     
-    books.push(book)
-
-    display(books)
-}
-
-function display(books){
-    let target = document.getElementById('target')
-
-    for(let book of books){
-        let title = document.createElement('p')
-        let author = document.createElement('p')
-        let button = document.createElement('button')
-        
-        title.innerHTML = book.title
-        author.innerHTML = book.author
-        button.innerHTML = 'remove'
-        button.className = 'remove'
-
-        target.appendChild(title)
-        target.appendChild(author)
-        target.appendChild(button)
-
-    }
+    showBooks()
+    
+    localStorage.setItem('savedBooks', JSON.stringify(books))
 
 }
 
 
+console.log(books);
